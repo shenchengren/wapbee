ae54db79ca662e768740594e91a55b06f5296ecf
shenchengren <2181824782@qq.com>
2018-02-12 11:59:01 +0800

    xiugai

--- js/index.js
+++ js/index.js
@@ -20,29 +20,29 @@ var Game = {
 			style: "enemy2",
 			hp: "2",
 			score: "5",
-			scorePlus: 5,
+			scorePlus: 3,
 			speed: 4
 		},
 		em3: {
 			style: "enemy3",
-			hp: "3",
-			score: "10",
-			scorePlus: 8,
-			speed: 8
+			hp: "4",
+			score: "8",
+			scorePlus: 7,
+			speed: 6
 		},
 		em4: {
 			style: "enemy4",
-			hp: "6",
-			score: "18",
-			scorePlus: 12,
-			speed: 10
+			hp: "9",
+			score: "15",
+			scorePlus: 10,
+			speed: 8
 		},
 		em5: {
 			style: "enemy5",
-			hp: "10",
+			hp: "17",
 			score: "20",
 			scorePlus: 15,
-			speed: 12
+			speed: 8
 		}
 	},
 	checkpoint: [{ //创建数据
@@ -54,52 +54,52 @@ var Game = {
 		lottery:[20,20,10,50]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1"],
+		data: ["em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em4", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1"],
 		iNum: 10,
 		speedX: 7,
 		speedY: 10,
 		timer: 4000,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[25,25,10,40]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1"],
+		data: ["em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em4", "em1", "em4", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em5", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1"],
 		iNum: 10,
 		speedX: 10,
 		speedY: 20,
 		timer: 3000,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[30,30,10,30]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1"],
+		data: ["em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em5", "em2", "em5", "em2", "em2", "em2", "em2", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em1", "em5", "em1", "em1", "em1"],
 		iNum: 10,
 		speedX: 15,
 		speedY: 25,
 		timer: 2500,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[30,20,20,30]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1"],
+		data: ["em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em5", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em1"],
 		iNum: 10,
-		speedX: 25,
-		speedY: 40,
+		speedX: 20,
+		speedY: 30,
 		timer: 2000,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[30,25,30,15]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em4","em4","em4","em4","em4","em4","em4","em4","em4","em4","em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3","em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3","em3", "em3","em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2"],
+		data: ["em4","em4","em4","em4","em4","em4","em4","em4","em4","em4","em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em4", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3","em3", "em3", "em2", "em2", "em2", "em2", "em2", "em2","em2", "em3","em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2"],
 		iNum: 10,
-		speedX: 25,
-		speedY: 40,
+		speedX: 18,
+		speedY: 35,
 		timer: 1500,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[30,15,35,10]//彩蛋比例--柱子，血量，子弹，空
 	},
 	{
-		data: ["em4","em4","em4","em4","em4","em4","em4","em4","em4","em4","em3", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3","em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3","em3", "em3","em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2", "em2"],
+		data: ["em4","em4","em4","em4","em4","em4","em4","em5","em4","em4","em3", "em5", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em4", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em5", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em3", "em2","em2", "em2", "em3", "em3", "em2", "em2", "em2", "em5","em2", "em2","em2", "em2", "em3", "em2", "em3", "em2", "em2", "em2", "em2", "em2"],
 		iNum: 10,
-		speedX: 25,
-		speedY: 40,
+		speedX: 20,
+		speedY: 35,
 		timer: 1500,
-		lottery:[20,40,10,30]//彩蛋比例--柱子，血量，子弹，空
+		lottery:[40,20,40,0]//彩蛋比例--柱子，血量，子弹，空
 	}
 	],
 	airData: { //飞机数据
@@ -115,7 +115,6 @@ var Game = {
 	maxLevel: 5,
 	init: function () { //初始化数据
 		this.createScore();
-		// this.createTips();
 		this.createEnemy(this.checkpoint[this.level]);
 		this.createAir();
 		this.maxLevel = this.checkpoint.length;//自动获取最高关卡数量
